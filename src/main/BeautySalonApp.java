4652cafcc3237ea2c4719535ad62c0fa583f8acb
Nikola352 <nikolajolovic02@gmail.com>
2023-04-28 12:53:37 +0200

    Initial commit

---
+++ src/main/BeautySalonApp.java
@@ -0,0 +1,10 @@
+package main;
+
+public class BeautySalonApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author nikola
+ *
+ */
+module BeautySalon {
+}
